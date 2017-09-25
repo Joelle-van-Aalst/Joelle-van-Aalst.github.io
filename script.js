9bfd0482bf4def078ad1eb9418dac24971679b9f
Joëlle van Aalst <joelle.vanaalst@live.nl>
2017-09-25 15:22:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 /* Open when someone clicks on the span element */
 
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" 
-		      href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-	</head>
+<!DOCTYPE javascript>
+
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
 }
